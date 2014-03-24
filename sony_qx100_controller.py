9390832939a0b5c23ce6f646e69a6ab3e9c15978
Ioann Volkov <volkov.ioann@gmail.com>
2014-03-24 20:41:25 +0400

    Display liveview

--- sony_qx100_controller.py
+++ sony_qx100_controller.py
@@ -5,17 +5,41 @@
 
 import sys, json
 import http.client, urllib.parse
+import threading
 import base64, hashlib
 
-conn = None
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+lock = threading.Lock()
+
+app = QApplication(sys.argv)
+image = QImage()
+
+class ImageDisplay(QLabel):
+    def __init__(self):
+        QLabel.__init__(self)
+
+    def paintEvent(self, event):
+        global lock
+        lock.acquire()
+        try:
+            self.setPixmap(QPixmap.fromImage(image))
+        finally:
+            lock.release()
+        QLabel.paintEvent(self, event)
+
+imgDisplay = ImageDisplay()
+imgDisplay.setMinimumSize(640, 480)
+imgDisplay.show()
+
 pId = 0
 headers = {"Content-type": "text/plain", "Accept": "*/*", "X-Requested-With": "com.sony.playmemories.mobile"}
 
 AUTH_CONST_STRING = "90adc8515a40558968fe8318b5b023fdd48d3828a2dda8905f3b93a3cd8e58dc"
 METHODS_TO_ENABLE = "camera/setFlashMode:camera/getFlashMode:camera/getSupportedFlashMode:camera/getAvailableFlashMode:camera/setExposureCompensation:camera/getExposureCompensation:camera/getSupportedExposureCompensation:camera/getAvailableExposureCompensation:camera/setSteadyMode:camera/getSteadyMode:camera/getSupportedSteadyMode:camera/getAvailableSteadyMode:camera/setViewAngle:camera/getViewAngle:camera/getSupportedViewAngle:camera/getAvailableViewAngle:camera/setMovieQuality:camera/getMovieQuality:camera/getSupportedMovieQuality:camera/getAvailableMovieQuality:camera/setFocusMode:camera/getFocusMode:camera/getSupportedFocusMode:camera/getAvailableFocusMode:camera/setStillSize:camera/getStillSize:camera/getSupportedStillSize:camera/getAvailableStillSize:camera/setBeepMode:camera/getBeepMode:camera/getSupportedBeepMode:camera/getAvailableBeepMode:camera/setCameraFunction:camera/getCameraFunction:camera/getSupportedCameraFunction:camera/getAvailableCameraFunction:camera/setLiveviewSize:camera/getLiveviewSize:camera/getSupportedLiveviewSize:camera/getAvailableLiveviewSize:camera/setTouchAFPosition:camera/getTouchAFPosition:camera/cancelTouchAFPosition:camera/setFNumber:camera/getFNumber:camera/getSupportedFNumber:camera/getAvailableFNumber:camera/setShutterSpeed:camera/getShutterSpeed:camera/getSupportedShutterSpeed:camera/getAvailableShutterSpeed:camera/setIsoSpeedRate:camera/getIsoSpeedRate:camera/getSupportedIsoSpeedRate:camera/getAvailableIsoSpeedRate:camera/setExposureMode:camera/getExposureMode:camera/getSupportedExposureMode:camera/getAvailableExposureMode:camera/setWhiteBalance:camera/getWhiteBalance:camera/getSupportedWhiteBalance:camera/getAvailableWhiteBalance:camera/setProgramShift:camera/getSupportedProgramShift:camera/getStorageInformation:camera/startLiveviewWithSize:camera/startIntervalStillRec:camera/stopIntervalStillRec:camera/actFormatStorage:system/setCurrentTime"
 
-def postRequest(target, req):
-    global conn
+def postRequest(conn, target, req):
     global pId
     pId += 1
     req["id"] = pId
@@ -32,8 +56,7 @@ def postRequest(target, req):
     print(data, end = "\n\n")
     return data
 
-def exitWithError(message):
-    global conn
+def exitWithError(conn, message):
     print("ERROR: %s" % message)
     conn.close()
     sys.exit(1)
@@ -53,13 +76,15 @@ def downloadImage(url):
     else:
         print("ERROR: Could not download picture, error = [%d %s]" % (response.status, response.reason))
 
-def symb5(c):
-    s = str(c)
-    while len(s) < 5:
-        s = "0" + s
-    return s
+#def symb5(c):
+#    s = str(c)
+#    while len(s) < 5:
+#        s = "0" + s
+#    return s
 
 def liveviewFromUrl(url):
+    global image
+    global lock
     host, port, address, img_name = parseUrl(url)
     conn3 = http.client.HTTPConnection(host, port)
     conn3.request("GET", address)
@@ -74,14 +99,19 @@ def liveviewFromUrl(url):
             if jpegEnd != -1:
                 c += 1
                 buf += nextPart[:jpegEnd + 2]
-                with open("live_" + symb5(c) + ".jpg", "wb") as liveImg:
-                    liveImg.write(buf)
+                #with open("live_" + symb5(c) + ".jpg", "wb") as liveImg:
+                #    liveImg.write(buf)
+                lock.acquire()
+                try:
+                    image.loadFromData(buf)
+                finally:
+                    lock.release()
             if jpegStart != -1:
                 buf = nextPart[jpegStart:]
             else:
                 buf += nextPart
 
-if __name__ == "__main__":
+def communicationThread():
     #target = "/sony/camera"
     #target = "/sony/system"
     #target = "/sony/accessControl"
@@ -95,31 +125,37 @@ if __name__ == "__main__":
 
     conn = http.client.HTTPConnection("10.0.0.1", 10000)
 
-    resp = postRequest("camera", {"method": "getVersions", "params": []})
+    resp = postRequest(conn, "camera", {"method": "getVersions", "params": []})
     if resp["result"][0][0] != "1.0":
-        exitWithError("Unsupported version")
+        exitWithError(conn, "Unsupported version")
 
-    resp = postRequest("accessControl", {"method": "actEnableMethods", "params": [{"methods": "", "developerName": "", "developerID": "", "sg": ""}], "version": "1.0"})
+    resp = postRequest(conn, "accessControl", {"method": "actEnableMethods", "params": [{"methods": "", "developerName": "", "developerID": "", "sg": ""}], "version": "1.0"})
     dg = resp["result"][0]["dg"]
 
     h = hashlib.sha256()
     h.update(bytes(AUTH_CONST_STRING + dg, "UTF-8"))
     sg = base64.b64encode(h.digest()).decode("UTF-8")
 
-    resp = postRequest("accessControl", {"method": "actEnableMethods", "params": [{"methods": METHODS_TO_ENABLE, "developerName": "Sony Corporation", "developerID": "7DED695E-75AC-4ea9-8A85-E5F8CA0AF2F3", "sg": sg}], "version": "1.0"})
+    resp = postRequest(conn, "accessControl", {"method": "actEnableMethods", "params": [{"methods": METHODS_TO_ENABLE, "developerName": "Sony Corporation", "developerID": "7DED695E-75AC-4ea9-8A85-E5F8CA0AF2F3", "sg": sg}], "version": "1.0"})
 
-    #resp = postRequest("camera", {"method": "getStillSize", "params": [], "version": "1.0"})
-    #resp = postRequest("camera", {"method": "getSupportedStillSize", "params": [], "version": "1.0"})
-    #resp = postRequest("camera", {"method": "getAvailableStillSize", "params": [], "version": "1.0"})
+    #resp = postRequest(conn, "camera", {"method": "getStillSize", "params": [], "version": "1.0"})
+    #resp = postRequest(conn, "camera", {"method": "getSupportedStillSize", "params": [], "version": "1.0"})
+    #resp = postRequest(conn, "camera", {"method": "getAvailableStillSize", "params": [], "version": "1.0"})
 
-    #resp = postRequest("camera", {"method": "setStillSize", "params": ["20M", "3:2"], "version": "1.0"})
+    #resp = postRequest(conn, "camera", {"method": "setStillSize", "params": ["20M", "3:2"], "version": "1.0"})
 
-    resp = postRequest("camera", {"method": "stopLiveview", "params": [], "version": "1.0"})
+    resp = postRequest(conn, "camera", {"method": "stopLiveview", "params": [], "version": "1.0"})
 
-    resp = postRequest("camera", {"method": "startLiveview", "params": [], "version": "1.0"})
+    resp = postRequest(conn, "camera", {"method": "startLiveview", "params": [], "version": "1.0"})
     liveviewFromUrl(resp["result"][0])
 
-    resp = postRequest("camera", {"method": "actTakePicture", "params": [], "version": "1.0"})
+    resp = postRequest(conn, "camera", {"method": "actTakePicture", "params": [], "version": "1.0"})
     downloadImage(resp["result"][0][0])
 
     conn.close()
+
+if __name__ == "__main__":
+    communication = threading.Thread(target = communicationThread)
+    communication.start()
+
+    sys.exit(app.exec_())
