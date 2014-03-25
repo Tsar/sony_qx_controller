f3ffac526ceca29645a6cbe7a8be2e5ffe80b53a
Ioann Volkov <volkov.ioann@gmail.com>
2014-03-25 16:47:30 +0400

    Removed password of my device; chmod +x sony_qx_controller.py

--- sony_qx_controller.py
+++ sony_qx_controller.py
@@ -4,7 +4,6 @@
 # Supports AUTHORIZATION (!!!), which allows to use a lot of undocumented commands (such as setStillSize and others).
 
 # Before using this script: manually connect to Wi-Fi; set up IP 10.0.1.1, mask 255.0.0.0 (and if that's not enough - default gateway: 10.0.0.1).
-# Password of MY device: rjo1Tvr4
 # Note. Password can be taken from a text file in the internal memory of the device, connect it with USB and switch on for accessing this memory.
 
 # This script depends on PyQt4 for displaying liveview. On Ubuntu just run command: apt-get install python3-pyqt4
